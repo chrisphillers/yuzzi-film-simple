078417767644aedfda7d5d767ace2a73b4606674
chrisphillers <chrisphillers@gmail.com>
2025-03-18 18:44:05 +0000

    Prettier fix

--- frontend/src/app/__tests__/page.test.tsx
+++ frontend/src/app/__tests__/page.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react'
-import Home from '../page'
+import { render, screen } from '@testing-library/react';
+import Home from '../page';
 
 describe('Home', () => {
   it.skip('renders a heading', () => {
-    render(<Home />)
-    const heading = screen.getByRole('heading')
-    expect(heading).toBe(true)
-  })
-}) 
+    render(<Home />);
+    const heading = screen.getByRole('heading');
+    expect(heading).toBe(true);
+  });
+});
