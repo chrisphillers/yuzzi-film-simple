6789e946bc74d725a5f1302c0912fc48c7870bf1
chrisphillers <chrisphillers@gmail.com>
2025-03-19 21:43:03 +0000

    Update: Video hover button and style jump fix

--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -1,16 +1,59 @@
 'use client';
 import Vimeo from '@u-wave/react-vimeo';
+import { Box } from 'grommet';
+import { useState } from 'react';
 
 export const Hero = ({ title }: { title: string }) => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <Vimeo
-      id={title}
-      video="76979871"
-      autoplay={true}
-      muted={true}
-      loop={true}
-      controls={false}
-      responsive={true}
-    />
+    <Box
+      style={{
+        position: 'relative',
+        paddingTop: '56.25%', // 16:9 aspect ratio (9/16 = 0.5625)
+        width: '100%', // Maintains responsiveness
+        overflow: 'hidden',
+        backgroundColor: 'grey',
+      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <Box
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        {/* Play button overlay */}
+        {isHovered && (
+          <Box
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              zIndex: 10,
+              width: '0',
+              height: '0',
+              borderTop: '40px solid transparent',
+              borderBottom: '40px solid transparent',
+              borderLeft: '80px solid #2300ff', // Blue color for the play button
+              cursor: 'pointer',
+            }}
+          />
+        )}
+        <Vimeo
+          id={title}
+          video="76979871"
+          autoplay={true}
+          muted={true}
+          loop={true}
+          controls={false}
+          responsive={true}
+        />
+      </Box>
+    </Box>
   );
 };
